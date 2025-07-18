891c49b3791abddb26b209508c6d6ca33c8f451d
Ariffah104 <162397834+AgungSaputra231230003@users.noreply.github.com>
2025-07-18 19:43:36 +0700

    first commit

---
+++ infoKampus.js
@@ -0,0 +1,4 @@
+function lihatSelengkapnya() {
+    const profil = document.getElementById("profil-text");
+    profil.textContent += " UM Pontianak terus berkembang menjadi kampus unggulan dengan akreditasi yang baik, fasilitas lengkap, serta dosen-dosen yang profesional dan berdedikasi tinggi.";
+}
